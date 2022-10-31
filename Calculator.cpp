4ceb3fad2c8e8a3e0ed03992885d6c262b7ec3c2
Izabel Videnova <100083184+IzabelLV@users.noreply.github.com>
2022-10-31 15:58:50 +0200

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 
 int main() {
 
@@ -7,16 +7,18 @@ int main() {
 	char operation;
 
 	do {
-
+		
 		std::cout << "What do you want to do?\n";
 		std::cout << "1) Sum\n";
 		std::cout << "2) Subtract\n";
 		std::cout << "3) Multiplicate\n";
 		std::cout << "4) Divide\n";
+		std::cout << "5) Convert from binary to decimal\n";
 		std::cin >> operation;
 		system("cls");
 
 		switch (operation) {
+
 		case '1':
 			std::cout << "Please enter two numbers:\n";
 			std::cin >> a;
@@ -30,18 +32,30 @@ int main() {
 			std::cin >> b;
 			std::cout << a << "-" << b << "=" << a - b << std::endl;
 			break;
+
 		case '3':
 			std::cout << "Please enter two numbers:\n";
 			std::cin >> a;
 			std::cin >> b;
 			std::cout << a << "*" << b << "=" << a * b << std::endl;
 			break;
+
 		case '4':
 			std::cout << "Please enter two numbers:\n";
 			std::cin >> a;
 			std::cin >> b;
 			std::cout << a << "/" << b << "=" << a / b << std::endl;
 			break;
+
+		case '5': {
+			std::string c;
+			std::cout << "Enter a binary number:\n"; std::cin >> c;
+			int num = 0;
+			num = stoi(c, 0, 2);
+			std::cout << num << std::endl;
+		}
+			break;
+
 		default:
 			std::cout << "Error" << std::endl;
 			break;
