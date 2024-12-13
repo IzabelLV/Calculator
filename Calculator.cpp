1e09a4a7c3c0ae3eacd6eebb24d8901cf220fd92
Izabel <izabel.videnova03@gmail.com>
2024-12-13 19:07:34 +0200

    Optimization in code
    Made a full optimization in the code.  Added functions and fixed the issue with  option 6.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 #include <string>
 
+
+void printMessage()
+{
+	std::cout << std::endl;
+	std::cout << "What do you want to do?\n";
+	std::cout << "1) Sum\n";
+	std::cout << "2) Subtract\n";
+	std::cout << "3) Multiplicate\n";
+	std::cout << "4) Divide\n";
+	std::cout << "5) Convert from binary to decimal\n";
+	std::cout << "6) Quit\n";
+}
+
+void printTwoNumbers()
+{
+	std::cout << "Please enter two numbers:\n";
+}
+
+double Sum(double firstDigit, double secondDigit)
+{
+	return firstDigit + secondDigit;
+}
+
+double Subtract(double firstDigit, double secondDigit)
+{
+	return firstDigit - secondDigit;
+}
+
+double Multiplicate(double firstDigit, double secondDigit)
+{
+	return firstDigit * secondDigit;
+}
+
+double Divide(double firstDigit, double secondDigit)
+{
+	return firstDigit / secondDigit;
+}
+
+int binaryToDecimal()
+{
+	std::string digit;
+	std::cout << "Enter a binary number:\n"; 
+	std::cin >> digit;
+	return stoi(digit, 0, 2);
+}
+
 int main() {
 
 	double a, b;
@@ -8,60 +54,52 @@ int main() {
 
 	do {
 		
-		std::cout << "What do you want to do?\n";
-		std::cout << "1) Sum\n";
-		std::cout << "2) Subtract\n";
-		std::cout << "3) Multiplicate\n";
-		std::cout << "4) Divide\n";
-		std::cout << "5) Convert from binary to decimal\n";
+		printMessage();
 		std::cin >> operation;
 		system("cls");
 
-		switch (operation) {
-
+		switch (operation) 
+		{
 		case '1':
-			std::cout << "Please enter two numbers:\n";
-			std::cin >> a;
-			std::cin >> b;
-			std::cout << a << "+" << b << "=" << a + b << std::endl;
+		    printTwoNumbers();
+	        std::cin >> a;
+	        std::cin >> b;
+			std::cout << a << " + " << b << " = " << Sum(a, b) << std::endl;
 			break;
 
 		case '2':
-			std::cout << "Please enter two numbers:\n";
-			std::cin >> a;
-			std::cin >> b;
-			std::cout << a << "-" << b << "=" << a - b << std::endl;
+		    printTwoNumbers();
+	        std::cin >> a;
+	        std::cin >> b;
+			std::cout << a << " - " << b << " = " << Subtract(a, b) << std::endl;
 			break;
 
 		case '3':
-			std::cout << "Please enter two numbers:\n";
-			std::cin >> a;
-			std::cin >> b;
-			std::cout << a << "*" << b << "=" << a * b << std::endl;
+		    printTwoNumbers();
+	        std::cin >> a;
+	        std::cin >> b; 
+			std::cout << a << " * " << b << " = " << Multiplicate(a, b) << std::endl;
 			break;
 
 		case '4':
-			std::cout << "Please enter two numbers:\n";
-			std::cin >> a;
-			std::cin >> b;
-			std::cout << a << "/" << b << "=" << a / b << std::endl;
+		    printTwoNumbers();
+	        std::cin >> a;
+	        std::cin >> b;
+			std::cout << a << " / " << b << " = " << Divide(a, b) << std::endl;
 			break;
 
-		case '5': {
-			std::string c;
-			std::cout << "Enter a binary number:\n"; std::cin >> c;
-			int num = 0;
-			num = stoi(c, 0, 2);
-			std::cout << num << std::endl;
-		}
-			break;
+		case '5':
+		   std::cout << binaryToDecimal() << std::endl;
+           break;
+
+		case '6':
+		    std::cout << "Have a good day." << std::endl;
+            break;  
 
 		default:
-			std::cout << "Error" << std::endl;
+			std::cout << "Error, please enter again." << std::endl;
 			break;
 		}
-
-	} while (operation != 5);
-
-	system("pause>0");
+        
+	} while (operation != '6');
 }
