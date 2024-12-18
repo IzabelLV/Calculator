d07186da6ff5482ff9633d0e1989866b8379be82
Izabel <izabel.videnova03@gmail.com>
2024-12-18 21:21:08 +0200

    More optimizations
    User input validation has been added to ensure correct input handling. All functions have been improved with clear declarations and definitions for better structure and readability.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,5 +1,122 @@
 #include <iostream>
 #include <string>
+#include <limits>
+
+
+void printMessage();
+
+void printTwoNumbers();
+
+double Sum(double, double);
+
+double Subtract(double, double);
+
+double Multiplicate(double, double);
+
+double Divide(double, double);
+
+int binaryToDecimal();
+
+void errorMessageWhenWrongInput();
+
+
+int main() {
+
+	double a, b;
+	char operation;
+
+	do {
+		
+		printMessage();
+		std::cin >> operation;
+		system("cls");
+
+		switch (operation) 
+		{
+		case '1':
+		    printTwoNumbers();
+	        std::cin >> a;
+
+			while (!(std::cin >> a))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+            std::cin >> b;
+			while (!(std::cin >> b))
+			{
+                errorMessageWhenWrongInput();
+			}
+			
+			std::cout << a << " + " << b << " = " << Sum(a, b) << std::endl;
+			break;
+
+		case '2':
+		    printTwoNumbers();
+	        std::cin >> a;
+			while (!(std::cin >> a))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+            std::cin >> b;
+			while (!(std::cin >> b))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+			std::cout << a << " - " << b << " = " << Subtract(a, b) << std::endl;
+			break;
+
+		case '3':
+		    printTwoNumbers();
+	        std::cin >> a;
+            while (!(std::cin >> a))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+            std::cin >> b;
+			while (!(std::cin >> b))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+			std::cout << a << " * " << b << " = " << Multiplicate(a, b) << std::endl;
+			break;
+
+		case '4':
+		    printTwoNumbers();
+	        std::cin >> a;
+			while (!(std::cin >> a))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+            std::cin >> b;
+			while (!(std::cin >> b))
+			{
+                errorMessageWhenWrongInput();
+			}
+
+			std::cout << a << " / " << b << " = " << Divide(a, b) << std::endl;
+			break;
+
+		case '5':
+		   std::cout << binaryToDecimal() << std::endl;
+           break;
+
+		case '6':
+		    std::cout << "Have a good day." << std::endl;
+            break;  
+
+		default:
+			std::cout << "Error, please enter again." << std::endl;
+			break;
+		}
+        
+	} while (operation != '6');
+}
 
 
 void printMessage()
@@ -43,63 +160,31 @@ int binaryToDecimal()
 {
 	std::string digit;
 	std::cout << "Enter a binary number:\n"; 
+	while (true)
+	{
 	std::cin >> digit;
-	return stoi(digit, 0, 2);
-}
+	bool isValid = true;
 
-int main() {
-
-	double a, b;
-	char operation;
-
-	do {
-		
-		printMessage();
-		std::cin >> operation;
-		system("cls");
-
-		switch (operation) 
+	for (char ch : digit)
+	{
+		if (ch != '0' && ch != '1')
 		{
-		case '1':
-		    printTwoNumbers();
-	        std::cin >> a;
-	        std::cin >> b;
-			std::cout << a << " + " << b << " = " << Sum(a, b) << std::endl;
-			break;
-
-		case '2':
-		    printTwoNumbers();
-	        std::cin >> a;
-	        std::cin >> b;
-			std::cout << a << " - " << b << " = " << Subtract(a, b) << std::endl;
-			break;
-
-		case '3':
-		    printTwoNumbers();
-	        std::cin >> a;
-	        std::cin >> b; 
-			std::cout << a << " * " << b << " = " << Multiplicate(a, b) << std::endl;
-			break;
-
-		case '4':
-		    printTwoNumbers();
-	        std::cin >> a;
-	        std::cin >> b;
-			std::cout << a << " / " << b << " = " << Divide(a, b) << std::endl;
-			break;
-
-		case '5':
-		   std::cout << binaryToDecimal() << std::endl;
-           break;
-
-		case '6':
-		    std::cout << "Have a good day." << std::endl;
-            break;  
-
-		default:
-			std::cout << "Error, please enter again." << std::endl;
+			isValid = false;
 			break;
 		}
-        
-	} while (operation != '6');
+	}
+	if (isValid)
+	{
+	    return stoi(digit, 0, 2);
+	}
+
+	    std::cout << "Invalid binary number!\n";
+	}
+}
+
+void errorMessageWhenWrongInput()
+{
+    std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Please enter only digits!\n";
 }
